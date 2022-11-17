80f10c89e2c2286bc18b2e79a406b25e61cdf549
Gbless <gbless1720@gmail.com>
2022-11-17 17:24:35 +0100

    LinkedList Trial

---
+++ App.py
@@ -0,0 +1,14 @@
+from LinkedListDS.LinkedList import LinkedList;
+
+linkedList = LinkedList()
+
+linkedList.insertEnd(12)
+linkedList.insertEnd(122)
+linkedList.insertEnd(3)
+linkedList.insertEnd(31)
+
+linkedList.traverseList()
+
+linkedList.remove(12)
+ 
+linkedList.traverseList()
